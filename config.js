e7b700e8d028f1a58601be696ea68a302eb3cb29
Muhammad Isrim <isrim@doogether.id>
2020-10-08 10:41:35 +0700

    riset

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const env = process.env.NODE_ENV || 'production'
 //insert your API Key & Secret for each environment, keep this file local and never push it to a public repo for security purposes.
 const config = {
 	development :{
-		APIKey : '',
-		APISecret : ''
+		APIKey : 'ZCxgUpZbTne6-wsTEV-zPA',
+		APISecret : 'DypwS1s6HI5IvtTYvb0sgMyLnBEY2g44Nyd6'
 	},
 	production:{	
-		APIKey : '',
-		APISecret : ''
+		APIKey : 'ZCxgUpZbTne6-wsTEV-zPA',
+		APISecret : 'DypwS1s6HI5IvtTYvb0sgMyLnBEY2g44Nyd6'
 	}
 };
 
